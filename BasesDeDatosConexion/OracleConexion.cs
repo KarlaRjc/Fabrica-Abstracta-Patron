63a7d183f5fe2ca567a79aed575b92facb8eb816
eviltwin666 <kcamposr021@ulacit.ed.cr>
2019-11-17 16:54:17 -0600

    Added class oracle conexion

---
+++ BasesDeDatosConexion/OracleConexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.BasesDeDatosConexion
+{
+    class OracleConexion:Conexion
+    {
+        public OracleConexion() {
+
+        }
+        public override String descripcion()
+        {
+            return "Conectado a la Base de Datos MYSQL";
+        }
+
+
+    }
+}
