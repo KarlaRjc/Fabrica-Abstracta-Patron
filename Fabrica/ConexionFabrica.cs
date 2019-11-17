4488d0bafde62e022e401a25c02dc29aaeb1d67e
eviltwin666 <kcamposr021@ulacit.ed.cr>
2019-11-17 16:51:32 -0600

    importing proyect

---
+++ Fabrica/ConexionFabrica.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractFactory.BasesDeDatosConexion;
+namespace AbstractFactory.Fabrica
+{
+    class ConexionFabrica
+    {
+        protected String tipoConexion;
+
+        public ConexionFabrica(String tipoConexion) {
+            this.tipoConexion = tipoConexion;
+        }
+
+        protected Conexion CreaConexion() {
+            if (tipoConexion.ToLower().Equals("Oracle")) {
+               //retorna conexion con oracle
+            }
+            else if (tipoConexion.ToLower().Equals("SQLServer"))
+            {
+                return new SQLServerConexion();
+            }
+            else if (tipoConexion.ToLower().Equals("PostgreSQL"))
+            {
+              //retorna conexion con PostgreSQL
+            }
+            else
+            {
+                return new MySQLConexion();
+            }
+          
+        }
+    }
+}
